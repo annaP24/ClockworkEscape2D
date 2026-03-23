1433aa9f035c2f50091b28d754ce6c450809d1f7
Ana <pavlovic.ana24@gmail.com>
2026-03-23 23:01:30 +0100

    exit

--- clockworkescape2d/scenes/levels/exit.gd
+++ clockworkescape2d/scenes/levels/exit.gd
@@ -5,20 +5,22 @@ signal level_finished
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var area: Area2D = $Area2D
 @onready var sparks: CPUParticles2D = $ExitSparks
-
+@onready var marker_2d: Marker2D = $Marker2D
+@onready var timer: Timer = $Timer
+var timer_timeout : float = 1.0
 
 func _ready():
 	randomize()
 	#activate()
 	start_pulsing()
-	burst_loop()
+	#burst_loop()
 
 func start_pulsing():
 	# Soft breathing glow on the icon
 	sprite.self_modulate = Color(1, 1, 1, 1)
 	var tw := create_tween().set_loops()
-	tw.tween_property(sprite, "self_modulate", Color(1.4, 1.2, 1.0, 1.0), 1.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
-	tw.tween_property(sprite, "self_modulate", Color(1.0, 1.0, 1.0, 1.0), 1.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tw.tween_property(sprite, "self_modulate", Color(1.4, 1.2, 1.0, 1.0), 2.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	tw.tween_property(sprite, "self_modulate", Color(1.0, 1.0, 1.0, 1.0), 2.0).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 
 func burst_loop() -> void:
 	await get_tree().process_frame
@@ -37,4 +39,13 @@ func activate():
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
-		level_finished.emit()
+		#ToDO: pull player
+		var tween = get_parent().create_tween()
+		tween.tween_property(body, "global_position", marker_2d.global_position, 1.0)
+		await tween.finished
+		body.process_mode = Node.PROCESS_MODE_DISABLED
+		timer.start(timer_timeout)
+
+
+func _on_timer_timeout() -> void:
+	level_finished.emit()
