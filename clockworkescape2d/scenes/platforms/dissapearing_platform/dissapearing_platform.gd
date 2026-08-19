17b860da9724affc36f4ddc68c906858cb2c835f
Ana <pavlovic.ana24@gmail.com>
2026-08-20 00:02:48 +0200

    Fixed level 19 and dissapearing platform

--- clockworkescape2d/scenes/platforms/dissapearing_platform/dissapearing_platform.gd
+++ clockworkescape2d/scenes/platforms/dissapearing_platform/dissapearing_platform.gd
@@ -13,6 +13,15 @@ func _ready() -> void:
 	collision.disabled = false
 	sprite_2d.texture = load("res://scenes/platforms/dissapearing_platform/assets/breakable_platform.png")
 
+func disable_collison() -> void:
+	detection_collision.disabled = true
+	collision.disabled = true
+	is_platform_visible = false
+
+func enable_collision() -> void:
+	collision.disabled = false
+	detection_collision.disabled = false
+	is_platform_visible = true
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
@@ -24,14 +33,7 @@ func _on_detection_area_body_entered(body: Node2D) -> void:
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "dissolve":
-		collision.disabled = true
-		detection_collision.disabled = true
-		is_platform_visible = false
 		timeout_timer.start(timeout)
-	elif anim_name == "appear":
-		is_platform_visible = true
-		collision.disabled = false
-		detection_collision.disabled = false
 
 func _on_appear_timer_timeout() -> void:
 	if is_platform_visible:
